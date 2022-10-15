7cdb9d4474a7724e0863f8065646d2780141a57b
Dobromir Kovachev <dobromir.mail@gmail.com>
2022-10-15 12:39:58 +0300

    Make ToolTip Plugin dinamic update and accept all the options from Tippy

--- src/toolTipPlugin/index.js
+++ src/toolTipPlugin/index.js
@@ -1,4 +1,8 @@
+// import ToolTip from './ToolTip.vue';
+import { defineAsyncComponent } from 'vue';
+
 export function toolTipPlugin(app, options) {
+    app.component("ToolTip", defineAsyncComponent(()=> import ("./ToolTip.vue")));
     console.log("Hello plugin", options)
 }
 
